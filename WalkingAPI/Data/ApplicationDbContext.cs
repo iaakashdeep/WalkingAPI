e5211944bc4bfd59bf538c9c5a37d9fd00575b00
iaakashdeep <deep.0812@gmail.com>
2024-09-04 19:48:11 +0530

    Commited base code

---
+++ WalkingAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WalkingAPI.Model.Domain;
+
+namespace WalkingAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+
+        //DbContext is the bridge between Model and DB, track changes and maintaining connections to DB
+
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+    }
+}
---
+++ WalkingAPI/Model/Domain/Walk.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WalkingAPI.Model.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthinKM {  get; set; }
+        public string? WalkImageUrl {  get; set; }
+
+        public Guid DifficultyId { get; set; }
+        [ForeignKey(nameof(DifficultyId))]
+        public Difficulty Difficulty { get; set; }
+
+        public Guid RegionId { get; set; }
+        [ForeignKey(nameof (RegionId))]
+        public Region Region { get; set; }
+    }
+}
